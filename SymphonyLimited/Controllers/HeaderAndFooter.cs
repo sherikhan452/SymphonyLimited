51dc67150df5091c63be8bc1137a578e3b99d482
Admin <sherikhan451@gmail.com>
2025-06-25 20:08:11 +0500

    G

---
+++ SymphonyLimited/Controllers/HeaderAndFooter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SymphonyLimited.Controllers
+{
+    public class HeaderAndFooter : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
